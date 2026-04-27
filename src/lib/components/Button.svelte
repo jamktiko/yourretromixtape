fd7d11297b8a08f4085c61b6a214159d6c2d108b
Wilho, Wilpola <AH4595@student.jamk.fi>
2026-04-27 09:56:20 +0300

    luotu button-komponentti

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,13 @@
+<!-- Yleinen button-komponentti. Näkyvinä ominausuuksina teksti ja mahdollinen disabled-tila -->
+<script lang="ts">
+	export interface Props {
+		onclick: () => void;
+		text: string;
+		disabled?: boolean;
+		// Napin ei-aktiivinen tila, joka estää käyttäjää klikkaamasta sitä. Oletuksena false, eli nappi on aktiivinen. Muista tyylitellä myös disabled-tila!
+	}
+
+	let { onclick, text, disabled = false }: Props = $props();
+</script>
+
+<button {onclick} {disabled}>{text}</button>

25585bcf652ff6db5916aa12bae8ca735cb8377c
nikom-hub <ah4518@student.jamk.fi>
2026-04-28 11:24:21 +0300

    mixedGenres button tyylitelty

--- src/lib/components/mixedGenres.svelte
+++ src/lib/components/mixedGenres.svelte
@@ -26,5 +26,7 @@
 	}
 </script>
 
-// buttonin ympärille div class, niin voi tyylitellä
-<button onclick={arvoVideo}>MixedGenres</button>
+<button
+	class="w-30 cursor-pointer rounded-lg border-[3px] border-text-color bg-black/0 p-2 font-teksti text-base leading-4 font-bold text-text-color shadow-[0px_4px_4px_2px_rgba(0,0,0,0.25)] backdrop-blur-md transition-all duration-300 ease-in-out hover:scale-105 hover:bg-black/0 hover:shadow-lg"
+	onclick={arvoVideo}>MixedGenres</button
+>

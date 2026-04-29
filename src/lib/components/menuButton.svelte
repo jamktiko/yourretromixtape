dba28b92757f4efb17eed61a5b070101a7eaa001
WilpolaWilho <AH4595@student.jamk.fi>
2026-04-29 13:18:02 +0300

    footer ja etusivun start-nappi

---
+++ src/lib/components/menuButton.svelte
@@ -0,0 +1,12 @@
+<!-- Yleinen button-komponentti. Näkyvinä ominausuuksina teksti ja mahdollinen disabled-tila -->
+<script lang="ts">
+	export interface Props {
+		onclick: () => void;
+		text: string;
+		// Napin ei-aktiivinen tila, joka estää käyttäjää klikkaamasta sitä. Oletuksena false, eli nappi on aktiivinen. Muista tyylitellä myös disabled-tila!
+	}
+
+	let { onclick, text }: Props = $props();
+</script>
+
+<button {onclick}>{text}</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,44 @@
 <script lang="ts">
 	import Genre from './Genre.svelte';
 	import MixedGenres from '$lib/components/mixedGenres.svelte';
+	import Button from '$lib/components/Button.svelte';
+	import { goto } from '$app/navigation';
+
+	async function arvoVideo() {
+		try {
+			const vastaus = await fetch('/data/biisit.json'); //hakee biisin
+			//await odottaa, että fetch on valmis ja tallentaa vastauksen muuttujaan vastaus
+			const data = await vastaus.json();
+
+			const suodatetutBiisit = data.filter(
+				(biisi: { genre: string }) => biisi.genre === valittuGenre
+			);
+
+			if (suodatetutBiisit.length === 0) {
+				console.error('Kyseisellä genrellä ei löytynyt biisejä:', valittuGenre);
+				return;
+			}
+
+			// Arpoo paikan listalta (0- listan pituus)
+			const randomID = Math.floor(Math.random() * suodatetutBiisit.length);
+			// Ottaa koko biisin teidot tuosta kohdasta
+			const valittuVideo = suodatetutBiisit[randomID];
+
+			// (?id=), koska silloin ei tarvitse [id]-kansiorakennetta.
+			const osoite = `new?id=${valittuVideo.id}`;
+
+			// eslint-disable-next-line svelte/no-navigation-without-resolve
+			await goto(osoite);
+		} catch (virhe) {
+			console.error('Musiikkivideon haku epäonnistui:', virhe);
+		}
+	}
+
+	let valittuGenre = $state('');
+
+	let onkoValittu = $derived(valittuGenre !== '');
 </script>
 
-<div><Genre /></div>
+<div><Genre bind:valittuGenre /></div>
 <MixedGenres />
+<Button onclick={arvoVideo} text="Start" disabled={!onkoValittu} />
